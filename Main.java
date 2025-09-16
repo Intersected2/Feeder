7e8d0d66bf2c5dc723fb84f1eaa4c272368e5d29
Intersected2 <brianx8@nycstudents.net>
2025-09-16 17:43:43 -0400

    feeder

--- Main.java
+++ Main.java
@@ -2,6 +2,11 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Feeder f = new Feeder();
+        Feeder f = new Feeder(1000);
+        System.out.println(f);
+        System.out.println((int) ((Math.random()*41 + 10)));
+        f.simulateOneDay(22);
+        System.out.println(f.getCurrentFood());
+        //System.out.println(Math.random() < 0.05);
     }
 }
